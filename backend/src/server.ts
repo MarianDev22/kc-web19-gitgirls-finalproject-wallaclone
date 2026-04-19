77927c3dd8f7173c10bd3a69782040acad7e0c9a
MarianDev22 <marianmfalicante@gmail.com>
2026-04-19 20:55:06 +0200

    ci/cd: 2nd test

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -3,7 +3,8 @@ import express from 'express';
 const app = express();
 
 app.use('/api', (req, res) => {
-  const message = '<h1> Este es el backend de las Git Girls ;) </h1>';
+  const message =
+    '<h1> Este es el backend de las Git Girls ;) - 2nd test github action CI/CD </h1>';
   res.send(message);
   //     res.json({
   //     info: 'first endpoint',
