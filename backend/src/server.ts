52623409ac335ea9009650a9408c723e543c8cf3
MarianDev22 <marianmfalicante@gmail.com>
2026-04-19 20:51:26 +0200

    ci: add github actions deploy workflow

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -3,7 +3,7 @@ import express from 'express';
 const app = express();
 
 app.use('/api', (req, res) => {
-  const message = '<h1> Hello World </h1>';
+  const message = '<h1> Este es el backend de las Git Girls ;) </h1>';
   res.send(message);
   //     res.json({
   //     info: 'first endpoint',
