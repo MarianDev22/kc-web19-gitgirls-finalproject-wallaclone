b52bfa5669f6d6240d6b369909ab0e121bae8b96
MarianDev22 <marianmfalicante@gmail.com>
2026-05-01 21:48:41 +0200

    fix: add authMW to route

--- backend/src/routes/webRoutes.ts
+++ backend/src/routes/webRoutes.ts
@@ -4,10 +4,9 @@ import { getAdsController } from '../controllers/products/getAdsController';
 import { authMiddleware } from '../middlewares/authMiddleware';
 import { deleteAdvertController } from '../controllers/products/deleteAdController';
 
-
 export const webRouter = express.Router();
 
 // Product/Ad Routes
 webRouter.post('/', authMiddleware, createAdvertController);
 webRouter.get('/', getAdsController);
-webRouter.delete('/:id', deleteAdvertController);
+webRouter.delete('/:id', authMiddleware, deleteAdvertController);
